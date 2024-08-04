3b367ef6e346f3815eab2b6c9ff48ba1d5c06d95
Guilherme Seletti <guiseletti@gmail.com>
2024-08-04 13:55:30 +0200

    CreateButtinsFetchingDataFromServiceWorker

--- popup.js
+++ popup.js
@@ -2,21 +2,25 @@ console.log("from popup...");
 
 //############################# Message Center ####################################
 
-async function sendMessageToContentScript(
-  _action,
-  _content = null,
-  _option = null
-) {
-  chrome.runtime.sendMessage(
-    { action: _action, Text: _content, Option: _option },
-    function (response) {
-      console.log("Response from background.js:", response);
-    }
-  );
+async function sendMessageToServiceWorker(_action, _content = null, _option = null) {
+  return new Promise((resolve, reject) => {
+    chrome.runtime.sendMessage(
+      { action: _action, Text: _content, Option: _option },
+      function (response) {
+        if (chrome.runtime.lastError) {
+          reject(new Error(chrome.runtime.lastError.message));
+        } else {
+          resolve(response);
+        }
+      }
+    );
+  });
 }
+
 
 //############################# Notification Center ####################################
 
+// TODO: Create dinamic notifications
 document
   .getElementById("notificationIcon")
   .addEventListener("click", function () {
@@ -31,33 +35,36 @@ document
 
 //############################# Dinamic Buttons ####################################
 
+// TODO: change the dataUpdated to DOMContentLoaded
 document.addEventListener('dataUpdated', createButtons);
 
-async function createButtons() {
-  fetch("dataBase/automationScriptsDb.json")
-    .then((response) => response.json())
-    .then((data) => {
-      // Extract the entries, sort them by textContent
-      const sortedEntries = Object.entries(data).sort((a, b) => {
-        return a[1].textContent.localeCompare(b[1].textContent);
-      });
-      // create button
-      sortedEntries.forEach(function ([key, value]) {
-        const newButton = document.createElement("button");
-        newButton.type = "button";
-        newButton.className = "btn btn-secondary btn-sm btn-spacing"; // Add btn-spacing class
-        newButton.id = key;
-        newButton.textContent = value.textContent;
-        document.getElementById(value.divIdToAppend).appendChild(newButton);
-        newButton.addEventListener("click", async () => {
-          sendMessageToContentScript("automation", null, key);
+function createButtons() {
+  sendMessageToServiceWorker("fetchAutomationButtons")
+    .then((response) => {
+      // Check if response is valid
+      if (response && typeof response === 'object') {
+        const sortedEntries = Object.entries(response).sort((a, b) => {
+          return a[1].textContent.localeCompare(b[1].textContent);
         });
-      });
+        // Create buttons
+        sortedEntries.forEach(function ([key, value]) {
+          const newButton = document.createElement("button");
+          newButton.type = "button";
+          newButton.className = "btn btn-secondary btn-sm btn-spacing"; // Add btn-spacing class
+          newButton.id = key;
+          newButton.textContent = value.textContent;
+          document.getElementById(value.divIdToAppend).appendChild(newButton);
+          newButton.addEventListener("click", async () => {
+            sendMessageToServiceWorker("automation", null, key);
+          });
+        });
+      } else {
+        console.error("Invalid response received from background script:", response);
+      }
     })
     .catch((error) => {
-      console.error('Error fetching data to build buttons', error);
+      console.error("Error in sendMessageToServiceWorker:", error);
     });
 }
 
-// Example trigger for dataUpdated event
 document.dispatchEvent(new Event('dataUpdated'));
--- service_worker.js
+++ service_worker.js
@@ -1,19 +1,32 @@
-console.log("from background...");
+console.log("from service_worker...");
 
-// receive messages from all parts
-chrome.runtime.onMessage.addListener(async function (
-  request,
-  sender,
-  sendResponse
-) {
+//############################# Message Center ####################################
+
+// Set up the onMessage listener
+chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
   if (request.action === "automation") {
-    console.log("chamei a funcao automation", request.Option)
+    console.log("chamei a funcao automation", request.Option);
     automation(request.Option);
+    sendResponse(true);
+  } else if (request.action === "fetchAutomationButtons") {
+    fetchAutomationButtons()
+      .then((data) => {
+        sendResponse(data); // Send the fetched data
+      })
+      .catch((error) => {
+        sendResponse({ error: error.message });
+      });
+    return true; // Indicate that sendResponse will be called asynchronously
   }
 });
 
+
 // send message to scripts
-async function sendMessageToContentScript(_action, _content = null, _object = null) {
+async function sendMessageToContentScript(
+  _action,
+  _content = null,
+  _object = null
+) {
   return new Promise((resolve, reject) => {
     chrome.tabs.query(
       { active: true, currentWindow: true },
@@ -23,7 +36,7 @@ async function sendMessageToContentScript(_action, _content = null, _object = nu
           {
             action: _action,
             Text: _content,
-            Object: _object
+            Object: _object,
           },
           function (response) {
             // Resolve the promise with the response
@@ -35,20 +48,30 @@ async function sendMessageToContentScript(_action, _content = null, _object = nu
   });
 }
 
-// TODO: make an env for this path  
+
+//############################# Fetch Center ####################################
+
+// TODO: make an env for this path
 async function automation(_key) {
   fetch("dataBase/automationScriptsDb.json")
     .then((response) => response.json())
     .then(async (data) => {
-      if (data[_key] && data[_key].script) { // Check if data[_key] and data[_key].script are defined
+      if (data[_key] && data[_key].script) {
+        // Check if data[_key] and data[_key].script are defined
         const scriptArray = data[_key].script;
         for (let i = 0; i < scriptArray.length; i++) {
           console.log("service_worker automationPers.:", scriptArray[i]);
-          await new Promise((resolve) => setTimeout(resolve, scriptArray[i].timeOut || 0));
+          await new Promise((resolve) =>
+            setTimeout(resolve, scriptArray[i].timeOut || 0)
+          );
           sendMessageToContentScript("automation", null, scriptArray[i]);
         }
       } else {
-        console.error("Data for key", _key, "or data[_key].script is undefined.");
+        console.error(
+          "Data for key",
+          _key,
+          "or data[_key].script is undefined."
+        );
       }
     })
     .catch((error) => {
@@ -56,4 +79,14 @@ async function automation(_key) {
     });
 }
 
-
+async function fetchAutomationButtons() {
+  try {
+    const response = await fetch("dataBase/automationScriptsDb.json");
+    const data = await response.json();
+    console.log("response", data);
+    return data; // Assuming `data.Object` is the correct structure
+  } catch (error) {
+    console.error("Error fetching automation buttons:", error);
+    throw new Error("Failed to fetch automation buttons");
+  }
+}
