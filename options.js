841e873360bfb2fb4a082c9f3db1d3dfe7fb6158
Guilherme Seletti <guiseletti@gmail.com>
2024-08-06 17:30:40 +0200

    OptionsAddNewLineImplementation

--- options.js
+++ options.js
@@ -1,5 +1,45 @@
-document.getElementById("submit").addEventListener("click", () => {
-    let newLine = document.createElement('section');
-    newLine.innerHTML = <button type="submit" class="btn btn-primary">Submit</button>
-    document.getElementById("automation").appendChild(newLine);
-})
+//########################## Form Section ######################################
+
+let sectionCount = 0;
+
+function cloneNode() {
+    sectionCount++;
+    const originalSection = document.querySelector('.automation-section');
+    const newSection = originalSection.cloneNode(true);
+    newSection.id = 'automation' + sectionCount;
+
+    document.getElementById('automation').appendChild(newSection);
+}
+
+document.getElementById("addNewLine").addEventListener('click', cloneNode);
+
+function deleteLastNode(){
+    const allSection = document.querySelectorAll('.automation-section');
+    if (allSection.length > 1){
+        const lastSection = allSection[allSection.length - 1];
+        lastSection.parentNode.removeChild(lastSection);
+    }
+}
+
+//########################## Push Section ######################################
+
+document.getElementById("submit").addEventListener('click', (event) => {
+    event.preventDefault();
+    const sections = document.querySelectorAll('.automation-section');
+    const data = [];
+
+    sections.forEach(section => {
+        const command = section.querySelector('.command').value;
+        const selectorType = section.querySelector('.selector-type').value;
+        const identifier = section.querySelector('.identifier').value;
+        const innerText = section.querySelector('.inner-text').value;
+        const input = section.querySelector('.input').value;
+        const timeout = section.querySelector('.timeout').value;
+        const color = section.querySelector('.color').value;
+
+        data.push({ command, selectorType, identifier, innerText, input, timeout, color });
+    });
+
+    console.log(data);
+    // Here you can send 'data' to your database
+});
